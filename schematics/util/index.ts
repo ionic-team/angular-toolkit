d421b2f3f4a32e48cfd0ff07d2bd5e8c779da8fd
Mike Hartington <mikehartington@gmail.com>
2019-02-05 16:50:08 -0500

    chore(): fix import path

--- schematics/util/index.ts
+++ schematics/util/index.ts
@@ -1,4 +1,4 @@
-import { strings } from '@angular-devkit/architect/node_modules/@angular-devkit/core';
+import { strings } from '@angular-devkit/core';
 
 export function buildSelector(options: any, projectPrefix: string) {
   let selector = strings.dasherize(options.name);
