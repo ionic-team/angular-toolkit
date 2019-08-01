4159e598a43bdedaaaa4d179dd7c3fabc2618d42
Ken Sodemann <ken@ionic.io>
2019-08-01 10:36:33 -0500

    fix(unit-tests): allow the components to hydrate (#173)

--- schematics/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
+++ schematics/component/files/__name@dasherize@if-flat__/__name@dasherize__.component.spec.ts
@@ -1,5 +1,5 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
 
 import { <%= classify(name) %>Component } from './<%= dasherize(name) %>.component';
 
@@ -10,16 +10,13 @@ describe('<%= classify(name) %>Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ <%= classify(name) %>Component ],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
-  }));
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(<%= classify(name) %>Component);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
