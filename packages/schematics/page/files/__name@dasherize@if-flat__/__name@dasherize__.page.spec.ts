b952e40c88905e150ee75e9915ace30f73238e1e
Mike Hartington <mikehartington@gmail.com>
2023-03-23 12:44:07 -0400

    feat(): add support for standalone pages this introduces a new standalone flag which can generate a standalone page for routing

--- packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.spec.ts
+++ packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.spec.ts
@@ -1,18 +1,11 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { <%= classify(name) %>Page } from './<%= dasherize(name) %>.page';
 
 describe('<%= classify(name) %>Page', () => {
   let component: <%= classify(name) %>Page;
   let fixture: ComponentFixture<<%= classify(name) %>Page>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ <%= classify(name) %>Page ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(<%= classify(name) %>Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.ts
+++ packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';<% if(routePath) { %>
-import { ActivatedRoute, Params } from '@angular/router';<% } %>
+import { ActivatedRoute, Params } from '@angular/router';<% } %><% if(standalone) {%>
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';<%} %>
 
 @Component({
   selector: '<%= selector %>',
   templateUrl: './<%= dasherize(name) %>.page.html',
-  styleUrls: ['./<%= dasherize(name) %>.page.<%= styleext %>'],
+  styleUrls: ['./<%= dasherize(name) %>.page.<%= styleext %>'],<% if(standalone) {%>
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]<%} %>
 })
 export class <%= classify(name) %>Page implements OnInit {<% if(routePath) { %>
 
