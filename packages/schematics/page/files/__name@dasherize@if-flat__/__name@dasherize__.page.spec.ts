e96a61862ac3fe2979a34a8826b01de712babb63
Masahiko Sakakibara <sakakibara@rdlabo.jp>
2023-05-29 10:41:22 +0900

    fix(template): fix page component test

--- packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.spec.ts
+++ packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.spec.ts
@@ -1,11 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { <%= classify(name) %>Page } from './<%= dasherize(name) %>.page';
 
 describe('<%= classify(name) %>Page', () => {
   let component: <%= classify(name) %>Page;
   let fixture: ComponentFixture<<%= classify(name) %>Page>;
 
-  beforeEach(async () => {
+  beforeEach(waitForAsync () => {
     fixture = TestBed.createComponent(<%= classify(name) %>Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
