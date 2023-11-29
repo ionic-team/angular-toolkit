8d6ce1c4b7622ec4afa746bf8d97a87f668b61bd
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2023-11-29 11:53:47 -0500

    feat(schematics): use Ionic standalone components (#504)
    resolves #500

--- packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.ts
+++ packages/schematics/page/files/__name@dasherize@if-flat__/__name@dasherize__.page.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';<% if(routePath) { %>
 import { ActivatedRoute, Params } from '@angular/router';<% } %><% if(standalone) {%>
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';<%} %>
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';<%} %>
 
 @Component({
   selector: '<%= selector %>',
   templateUrl: './<%= dasherize(name) %>.page.html',
   styleUrls: ['./<%= dasherize(name) %>.page.<%= styleext %>'],<% if(standalone) {%>
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]<%} %>
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]<%} %>
 })
 export class <%= classify(name) %>Page implements OnInit {<% if(routePath) { %>
 
